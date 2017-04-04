31db6cea01152a03d395b05fa8ae1b93f3fabb7e
Stuart Welham <stuart.welham@gmail.com>
2017-04-04 09:56:03 +0000

    added basic files interface

---
+++ lib/ivar/files.ex
@@ -0,0 +1,56 @@
+defmodule Ivar.Files do
+  @moduledoc """
+  Ivar.Files manages the files to be sent for a request
+  """
+  
+  @valid_types [nil, :multipart, :url_encoded]
+  
+  @doc """
+  Puts the given `files` into the existing `request` map
+  
+  Args
+  
+  * `request` - the map used to store the map of headers, usually created via `Ivar.new/2`
+  * `files` - the files to be put into the files list
+  
+  Files
+  
+  * tuple - `{"field_name", "some iodata", "file_name.txt", "text"}`
+  * list - a list of tuples in the same format as above
+  """
+  # Usage
+  #     iex> file_data = File.read!("test/fixtures/elixir.png")
+  #     iex> Ivar.Files.put(%{}, {"elixir", file_data, "elixir.png", "png"})
+  #     %{files: [{"elixir", , {"form-data", [{"name", "elixir"}, {"filename", "elixir.png"}]}, [{"content-type", "image/png"}]}]}
+  @spec put(map, {tuple | list}) :: map | {:error, binary}
+  def put(request, files) when is_tuple(files),
+    do: put(request, [files])
+  def put(request, files) do
+    if is_valid?(request) do
+      request
+      |> Map.get(:files, [])
+      |> put_files(files)
+      |> put_in_request(request)
+    else
+      {:error, "Files can only be put into a :url_encoded or :multipart body"}
+    end
+  end
+  
+  defp put_files(files, []), do: files
+  defp put_files(files, [{name, data, file_name, type} | rest]) do
+    file = {
+      name,
+      IO.iodata_to_binary(data),
+      {"form-data", [{"name", name}, {"filename", file_name}]},
+      [{"content-type", :mimerl.extension(type)}]
+    }
+    
+    put_files([file | files], rest)
+  end
+  
+  defp put_in_request(files, request),
+    do: Map.put(request, :files, files)
+  
+  defp is_valid?(%{body: body}) when body in @valid_types, do: true
+  defp is_valid?(request), do: request[:body] == nil
+end
---
+++ test/ivar/files_test.exs
@@ -0,0 +1,51 @@
+defmodule IvarTest.Files do
+  use ExUnit.Case, async: true
+  doctest Ivar.Files
+
+  alias Ivar.Files
+  
+  setup do
+    file_data = File.read!("test/fixtures/elixir.png")
+
+    {:ok, file_data: file_data}
+  end
+  
+  test "put/2 should put single file into request map", %{file_data: file_data} do
+    expect = file_component("test", file_data, "test.jpg", "image/jpeg")
+    
+    result = Files.put(%{}, {"test", file_data, "test.jpg", "jpg"})
+    
+    assert result == %{files: [expect]}
+  end
+  
+  test "put/2 should put multiple files into request map", %{file_data: file_data} do
+    expect = [
+      file_component("test2", "some plain text", "test2.txt", "text/plain"),
+      file_component("test", file_data, "test.jpg", "image/jpeg")
+    ]
+    
+    result = Files.put(%{}, [
+      {"test", file_data, "test.jpg", "jpg"},
+      {"test2", "some plain text", "test2.txt", "text"}
+    ])
+    
+    assert result == %{files: expect}
+  end
+  
+  test "put/2 should return error tuple when request is not url_encoded or multipart" do
+    result = %{}
+    |> Ivar.Body.put(%{test: 123}, :json)
+    |> Files.put({"test2", "some plain text", "test2.txt", "text"})
+    
+    assert result == {:error, "Files can only be put into a :url_encoded or :multipart body"}
+  end
+  
+  defp file_component(name, data, file_name, type) do
+    {
+      name,
+      data,
+      {"form-data", [{"name", name}, {"filename", file_name}]},
+      [{"content-type", type}]
+    }
+  end
+end
