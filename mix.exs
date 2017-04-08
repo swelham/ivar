9172a30d1950fa0707ab35c97ae62b7ae29dea4c
stuart.welham@gmail.com <stuart.welham@gmail.com>
2017-04-08 22:40:24 +0100

    inc version due to publish error

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ivar.Mixfile do
 
   def project do
     [app: :ivar,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
