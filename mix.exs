9db92e3d28acffb1cd0b84b04ebb59c369e06b05
Stuart Welham <stuart.welham@gmail.com>
2017-04-11 15:05:12 +0000

    inc version number to 0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ivar.Mixfile do
 
   def project do
     [app: :ivar,
-     version: "0.3.1",
+     version: "0.4.0",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
