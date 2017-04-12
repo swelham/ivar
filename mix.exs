3b35bf8a46c4af186505ac33c940d9398a088d9e
swelham <stuart.welham@gmail.com>
2017-04-12 10:13:24 +0100

    inc version to 0.5.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ivar.Mixfile do
 
   def project do
     [app: :ivar,
-     version: "0.4.0",
+     version: "0.5.0",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
