2620ca2147255924b2af94a0e785c6612bd27a47
swelham <stuart.welham@gmail.com>
2017-09-12 21:30:51 +0100

    fixed credo issues

--- lib/ivar/adapter.ex
+++ lib/ivar/adapter.ex
@@ -2,7 +2,7 @@ defmodule Ivar.Adapter do
   @moduledoc """
   Specifies API required for an Ivar adapter to implement.
   """
-  
+
   @doc """
   Callback used to execute the given `request`.
   """
